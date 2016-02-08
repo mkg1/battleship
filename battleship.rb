901260dcfbef0b57c5ccfe98fa0ed6104853a609
Molly G <gehring.mg@gmail.com>
2016-02-08 15:07:14 -0500

    Ship can be placed across or down

--- battleship.rb
+++ battleship.rb
@@ -20,4 +20,37 @@ class Ship
   def initialize (length)
     @length = length
   end
+
+  def place (x, y, across)
+    @x = x
+    @y = y
+    @across = across
+    if @across == true
+      @length.times do
+        covers?(x, y)
+        x += 1
+      end
+    elsif @across == false
+      @length.times do
+        covers?(x, y)
+        y += 1
+      end
+    end
+  end
+
+  def covers?(x, y)
+    if @across == true
+      if (x >= @x) && (x < (@x + @length)) && y == @y
+        return true
+      end
+    elsif @across == false
+      if (y >= @y) && (y < (@y + @length)) && x == @x
+        return true
+      end
+    end
+  end
 end
+
+ship = Ship.new(4)
+ship.place(2, 1, true)
+ship.covers?(4, 2)
