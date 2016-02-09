2bacdc65c64545412b33e0a05ba460b6cf5de1d1
Molly G <gehring.mg@gmail.com>
2016-02-09 18:02:51 -0500

    Add some puts statements for user

--- human_player.rb
+++ human_player.rb
@@ -1,5 +1,9 @@
 require './grid.rb'
 
+def get_user_input
+  gets.chomp
+end
+
 class HumanPlayer < Player
   attr_reader :name, :grid
   def initialize (name = "Dave")
@@ -7,4 +11,17 @@ class HumanPlayer < Player
     @name = name
   end
 
+  def place_ships(array)
+    puts "#{@name}, where would you like to place a ship of length #{array[0]}?"
+    puts "Across or Down?"
+    puts "#{@name}, where would you like to place a ship of length #{array[1]}?"
+    puts "Across or Down?"
+  end
+
+  def ships
+  end
+
+  def length
+  end
+  
 end
