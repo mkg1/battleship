dc5506821320bc75465b420c03fbb3bbe4d7f6e0
Molly G <gehring.mg@gmail.com>
2016-02-09 16:44:44 -0500

    Add sunk? method and display grid

--- grid.rb
+++ grid.rb
@@ -1,23 +1,45 @@
+require './player.rb'
+require './human_player.rb'
+require './computer_player.rb'
+require './grid.rb'
+require './ship.rb'
+require './position.rb'
+
 class Grid
   def initialize
     @ships = []
+    @hits = []
+    @total_ships = 0
+    @total_hits = 0
   end
 
   def display
-  puts "    1   2   3   4   5   6   7   8   9   10
-  -----------------------------------------
-A |   |   |   |   |   |   |   |   |   |   |
-B |   |   |   |   |   |   |   |   |   |   |
-C |   |   |   |   |   |   |   |   |   |   |
-D |   |   |   |   |   |   |   |   |   |   |
-E |   |   |   |   |   |   |   |   |   |   |
-F |   |   |   |   |   |   |   |   |   |   |
-G |   |   |   |   |   |   |   |   |   |   |
-H |   |   |   |   |   |   |   |   |   |   |
-I |   |   |   |   |   |   |   |   |   |   |
-J |   |   |   |   |   |   |   |   |   |   |
-  -----------------------------------------"
+    display_header
+    display_line
+    ("A".."J").each_with_index do |l, i|
+      y = i+1
+      line = l + " |"
+      (1..10).each do |x|
+        ship = has_ship_on?(x, y)
+        line << if ship && @hits.include?([x,y])
+          # @total_hits += 1
+          " X |"
+        elsif ship
+          " O |"
+        else
+          "   |"
+        end
+      end
+        puts line
+      end
+    display_line
+  end
 
+  private def display_line
+    puts "  -----------------------------------------"
+  end
+  private def display_header
+    puts "    1   2   3   4   5   6   7   8   9   10"
   end
 
   def has_ship_on?(x, y)
@@ -29,6 +51,47 @@ J |   |   |   |   |   |   |   |   |   |   |
 
   def place_ship(ship, x, y, across)
     ship.place(x, y, across)
-    @ship << ship
+    overlap = false
+    @ships.each do |p|
+      if p.overlaps_with? (ship)
+        overlap = true
+      end
+    end
+    unless overlap
+      @total_ships += ship.length
+      @ships << ship
+    end
+  end
+
+  def fire_at(x, y)
+    if @hits.include?([x, y])
+      return false
+    else
+      @total_hits += 1
+      @hits << [x, y]
+    end
+    if x > 10 || y > 10
+      return false
+    elsif has_ship_on?(x, y)
+      return true
+    else
+      return false
+    end
+  end
+
+  def sunk?
+    if @total_ships == @total_hits && @total_ships != 0
+      return true
+    end
+    false
   end
 end
+
+# grid = Grid.new
+# puts grid.sunk?
+# grid.place_ship(Ship.new(2), 6, 4, true)
+# puts grid.sunk?
+# grid.fire_at(6, 4)
+# puts grid.sunk?
+# grid.fire_at(7, 4)
+# puts grid.sunk?
--- ship.rb
+++ ship.rb
@@ -5,18 +5,17 @@ class Ship
   def initialize (length)
     @length = length
     @ship_coordinates = []
-    @hits = []
   end
 
   def place (x, y, across)
     if @ship_coordinates.empty?
       if across == true
         (x...@length+x).each do |i|
-          @ship_coordinates << [i, y]
+          @ship_coordinates << Position.new(i, y)
         end
       elsif across == false
         (y...@length+y).each do |j|
-          @ship_coordinates << [x, j]
+          @ship_coordinates << Position.new(x, j)
         end
       end
     else
@@ -26,23 +25,35 @@ class Ship
   end
 
   def covers?(x, y)
-    @ship_coordinates.include?([x, y])
+    @ship_coordinates.each do |p|
+      return p if x == p.x && y == p.y
+    end
+    false
   end
 
   def overlaps_with?(ship_object)
     @ship_coordinates.each do |p|
-      return true if ship_object.covers?(p[0], p[1])
+      return true if ship_object.covers?(p.x, p.y)
     end
     false
   end
 
   def fire_at(x, y)
-    if covers?(x, y) && !@hits.include?([x, y])
-      @hits << [x, y]
-    end
+    position = covers?(x, y)
+    position && position.hit!
+  end
+
+  def hit_on?(x, y)
+    position = covers(x, y)
+    position && position.hit?
   end
 
   def sunk?
-    @hits.length == length
+    return false if @ship_coordinates.empty?
+    all_hit = true
+    @ship_coordinates.each do |p|
+      all_hit = false if !p.hit?
+    end
+    all_hit
   end
 end
