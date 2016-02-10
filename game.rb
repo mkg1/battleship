789be0a30d1365755e3867d358cff85947ddf157
Molly G <gehring.mg@gmail.com>
2016-02-10 14:59:51 -0500

    Add place_ships method to game class

--- game.rb
+++ game.rb
@@ -8,4 +8,10 @@ class Game
   def welcome
     puts "Welcome, #{human.name} and #{computer.name}!\nIt's time to play Battleship.\n"
   end
+
+  def place_ships
+    ship_array = [2, 3, 3, 4, 5]
+    @human.place_ships(ship_array)
+    @computer.place_ships(ship_array)
+  end
 end
